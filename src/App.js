da863cc8df87f03fc42843d0954c89f834829446
vaidabernotiene <vaidabernotiene@yahoo.com>
2023-11-21 19:59:00 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+        <fieldset>
+            <legend>Personal info:</legend>
+            <label for="fname">First name:</label>
+            <input type="text" id="fname" name="fname" required maxlength="5"/><br></br>
+            <label for="lname">Last name:</label>
+            <input type="text" id="lname" name="lname" readonly/><br></br>
+            <label for="birthday">Birthday:</label>
+            <input type="date" id="birthday" name="birthday"/><br></br>
+            <label for="myfile">Select a file: </label>
+            <input type="file" name="myfile"/><br></br>
+            <select name="" id="" >
+                <option value="">1</option>
+                <option value="">2</option>
+            </select>
+            <button><strong>Submit</strong></button>
+            <button>Reset</button>
+        </fieldset>
+    </form>
       </header>
     </div>
   );
